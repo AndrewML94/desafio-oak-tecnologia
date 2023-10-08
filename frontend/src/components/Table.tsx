1a31f739543d44dc67ec02e162a7b802e4befdff
Andrew Lima <andrewmll94@gmail.com>
2023-10-08 13:05:02 -0300

    Refactor: Ajustes no código do componente Table e context FetchProductContext

--- frontend/src/components/Table.tsx
+++ frontend/src/components/Table.tsx
@@ -1,11 +1,17 @@
 import React, { useContext, useEffect, useState } from 'react';
+import { Store } from 'react-notifications-component';
 import { FetchProductContext } from '../context/FetchProductContext';
 import editProduct from '../assets/edit.png';
 import deleteProductImg from '../assets/delete.png';
 import CreateProduct from './CreateProduct';
 
 function Table() {
-  const { products, getProducts, deleteProduct } = useContext(FetchProductContext);
+  const {
+    products,
+    messageError,
+    getProducts,
+    deleteProduct,
+  } = useContext(FetchProductContext);
   const [isClicked, setIsClicked] = useState(false);
   const [isId, setId] = useState(0);
 
@@ -27,6 +33,19 @@ function Table() {
 
   return (
     <div>
+      {messageError && Store.addNotification({
+        title: 'Ocorreu um erro!',
+        message: `${messageError}`,
+        type: 'danger',
+        insert: 'top',
+        container: 'center',
+        animationIn: ['animate__animated', 'animate__fadeIn'],
+        animationOut: ['animate__animated', 'animate__fadeOut'],
+        dismiss: {
+          duration: 5000,
+          onScreen: true,
+        },
+      })}
       {
         products.length === 0 ? <p>Nenhum produto cadastrado</p> : (
           <table>
@@ -70,6 +89,13 @@ function Table() {
                         <tr>
                           <td>
                             <CreateProduct id={ elem.id } />
+                          </td>
+                          <td>
+                            <button
+                              onClick={ () => setIsClicked(!isClicked) }
+                            >
+                              Fechar edição
+                            </button>
                           </td>
                         </tr>
                       )
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ReactNotifications } from 'react-notifications-component';
 import App from './App.tsx';
 import FetchProductProvider from './context/FetchProductContext.tsx';
+import 'react-notifications-component/dist/theme.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <FetchProductProvider>
     <BrowserRouter>
+      <ReactNotifications />
       <App />
     </BrowserRouter>
   </FetchProductProvider>,
